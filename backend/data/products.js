b942c1ca312839dc03fad8a3ffa0920778527070
romi <romi@rominoMacBook-Pro.local>
2022-10-11 23:33:06 +0200

    backend User Authentication with token

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,12 +1,12 @@
 const products = [
   {
-    name: 'Airpods Wireless Bluetooth Headphones',
+    name: '古箪笥',
     image: '/images/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
     category: 'Electronics',
-    price: 89.99,
+    price: 59.99,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
