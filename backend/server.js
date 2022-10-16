dd1100331011c033cddc08ba1e60c2e488fe9c85
romi <romi@rominoMacBook-Pro.local>
2022-10-16 13:52:44 +0200

    prepare before HEROKU

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -16,14 +17,12 @@ connectDB()
 
 const app = express()
 
-if(process.env.NODE_ENV === 'developement') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
 app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -36,10 +35,23 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
+
 app.listen(
   PORT,
   console.log(
