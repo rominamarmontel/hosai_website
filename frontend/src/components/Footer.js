3338c8a7dcc1c36ea515a8a3181e6a5ea2e64838
romi <romi@rominoMacBook-Pro.local>
2022-10-20 23:52:22 +0200

    orderController

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,6 @@ const Footer = () => {
   return (
     <footer className="border-top pt-3">
       <Container>
-        <LinkContainer to="/">
-          <i className="bi bi-facebook"></i>
-        </LinkContainer>
         <Row>
           <Col className="text-center pb-0">
             <h4>Copyright &copy; KANRAKU HOSAI Website</h4>
